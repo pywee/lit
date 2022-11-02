a31b3b5929caa9df183bcdb58342e762b79ccf82
邱湘成 <xc.qiu@unipets.cn>
2022-11-02 14:31:36 +0800

    修复解析自定义函数形式参数时，未将其默认值放入函数体内解析

--- expr_logic.go
+++ expr_logic.go
@@ -5,12 +5,12 @@ import (
 	"github.com/pywee/lit/global"
 )
 
-func (r *Expression) parseAnd(expr []*global.Structure, k int, pos string) (*global.Structure, error) {
+func (r *Expression) parseAnd(expr []*global.Structure, k int, pos string, innerVariable map[string]*global.Structure) (*global.Structure, error) {
 	var (
 		err error
 		rv  *global.Structure
 	)
-	if rv, err = r.parse(expr[:k], pos, nil); err != nil {
+	if rv, err = r.parse(expr[:k], pos, innerVariable); err != nil {
 		return nil, err
 	}
 	// if rv.Tok != "IDENT" {
@@ -18,25 +18,25 @@ func (r *Expression) parseAnd(expr []*global.Structure, k int, pos string) (*glo
 	if _, ok := fn.ChangeToBool(rv); !ok {
 		return rv, nil
 	}
-	if rv, err = r.parse(expr[k+1:], pos, nil); err != nil {
+	if rv, err = r.parse(expr[k+1:], pos, innerVariable); err != nil {
 		return nil, err
 	}
 	fn.ChangeToBool(rv)
 	return rv, nil
 }
 
-func (r *Expression) parseOr(expr []*global.Structure, k int, pos string) (*global.Structure, error) {
+func (r *Expression) parseOr(expr []*global.Structure, k int, pos string, innerVariable map[string]*global.Structure) (*global.Structure, error) {
 	var (
 		err error
 		rv  *global.Structure
 	)
-	if rv, err = r.parse(expr[:k], pos, nil); err != nil {
+	if rv, err = r.parse(expr[:k], pos, innerVariable); err != nil {
 		return nil, err
 	}
 	if _, ok := fn.ChangeToBool(rv); ok {
 		return rv, nil
 	}
-	if rv, err = r.parse(expr[k+1:], pos, nil); err != nil {
+	if rv, err = r.parse(expr[k+1:], pos, innerVariable); err != nil {
 		return nil, err
 	}
 
