bfbf4394c1ff72ae0eeeb3d2057f18420b3f0c4f
pywee <3542000186@qq.com>
2022-11-13 23:58:01 +0800

    加入示例文件

---
+++ example/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	goExpr "github.com/pywee/lit"
+)
+
+func main() {
+	exprs, err := os.ReadFile("./src.lit")
+	if err != nil {
+		panic(err)
+	}
+	if _, err = goExpr.NewExpr(exprs); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
